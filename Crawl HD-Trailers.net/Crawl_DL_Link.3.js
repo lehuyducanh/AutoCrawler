97c85e5a50ac5a2c4f871eec82cdb21679d3de07
Le Huy Duc Anh <lehuyducanh@gmail.com>
2019-04-14 23:47:48 +0700

    Clear IMDB + HD Trailer

---
+++ Crawl HD-Trailers.net/Crawl_DL_Link.3.js
@@ -0,0 +1,45 @@
+const { Chromeless } = require('chromeless');
+const fs = require('fs');
+var MovieLink = fs.readFileSync('MovieLink.txt').toString().split("\n");
+
+async function run() {
+  const chromeless = new Chromeless({
+    lauchChrome: false,
+    waitTimeout: 60000000
+  });
+
+
+  //Go to Movie Link and Get Download Link
+
+  for (var i = 3717; i < 4000; i++) {
+    const MoviePage = await chromeless
+      .goto(MovieLink[i])
+      .evaluate(() => {
+        const MoviePage = [].map.call(
+          document.querySelectorAll('.bottomTableResolution a'),
+          a => a.href + '\n'
+        )
+        return MoviePage
+      })
+
+    //Get MovieName
+    const MovieName = await chromeless
+    .evaluate(() => {
+      const MovieName = [].map.call(
+        document.querySelectorAll('.previewTitle'),
+        a => a.innerText
+      )
+      return MovieName
+    })
+    MovieName[0] = MovieName[0].replace(/([^a-zA-Z0-9().&-\s])/g, '');
+
+    for (var j = 2; j < MoviePage.length; j+=3){
+      fs.appendFileSync(i + '. ' + MovieName[0] + '.txt', MoviePage[j]);
+    }
+
+  }
+ 
+  await chromeless.end();
+}
+run().catch(console.error.bind(console));
+
---
+++ Crawl HD-Trailers.net/Crawl_TV_Data.1.js
@@ -0,0 +1,30 @@
+const { Chromeless } = require('chromeless');
+const fs = require('fs');
+
+async function run() {
+  const chromeless = new Chromeless({
+    lauchChrome: false,
+    waitTimeout: 60000000
+  });
+
+
+  //Go to Movie Link and Get Actor Link
+var MovieLinkAll = ""
+  for (var i = 1; i < 517; i++) {
+    const MovieLink = await chromeless
+      .goto("http://www.hd-trailers.net/page/" + i)
+      .evaluate(() => {
+        const MovieLink = [].map.call(
+          document.querySelectorAll('.indexTableTrailerImage a'),
+          a => a.href + "\n"
+        )
+        return MovieLink
+      })
+      MovieLinkAll += MovieLink
+
+      }
+  fs.writeFileSync('MovieLink.txt', MovieLinkAll);
+  await chromeless.end();
+}
+run().catch(console.error.bind(console));
+
