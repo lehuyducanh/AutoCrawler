db48f9ac6ef488bb701df73fbbd8428a5948a924
Le Huy Duc Anh <lehuyducanh@gmail.com>
2019-05-24 10:28:26 +0700

    Update ShareAE 24-05-2019

--- Crawl ShareAE/CrawlDownloadPage.js
+++ Crawl ShareAE/CrawlDownloadPage.js
@@ -1,6 +1,6 @@
 const { Chromeless } = require('chromeless');
 const fs = require('fs');
-var InstaLink = fs.readFileSync('DownloadPage.txt').toString().split("\n");
+
 var AllDownloadLink = "";
 async function run() {
   const chromeless = new Chromeless({
@@ -9,12 +9,12 @@ async function run() {
   });
 
 
-  for (var i = 0; i < InstaLink.length; i++) {
+  for (var i = 1; i < 100; i++) {
     const DLLink = await chromeless
-      .goto(InstaLink[i])
+      .goto('https://shareae.com/after-effects-project/page/'+i)
       .evaluate(() => {
         const DLLink = [].map.call(
-          document.querySelectorAll('.downloadz a'),
+          document.querySelectorAll('.contenthead a'),
           a => a.href + '\n'
         )
    
@@ -22,7 +22,7 @@ async function run() {
       })
       AllDownloadLink += DLLink
   }
-  fs.appendFileSync("DownloadLink.txt", AllDownloadLink);
+  fs.appendFileSync("DownloadPage.txt", AllDownloadLink);
   await chromeless.end();
 }
 run().catch(console.error.bind(console));
